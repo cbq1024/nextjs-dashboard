37c418e7ee2ce94bc5dc5f7e4c3fc42112073e0c
cbq1024 <caobq1024@gmail.com>
2024-10-22 20:05:30 +0800

    feat: add eslint rules and config prettier

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,12 +1,13 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Image from "next/image";
+import { lusitana } from "@/app/ui/fonts";
 
-import { fetchLatestInvoices } from '@/app/lib/data';
+import { fetchLatestInvoices } from "@/app/lib/data";
 
-export default async function LatestInvoices() { // Remove props
-    const latestInvoices = await fetchLatestInvoices();
+export default async function LatestInvoices() {
+  // Remove props
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -15,15 +16,15 @@ export default async function LatestInvoices() { // Remove props
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-         <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
+                    "border-t": i !== 0,
                   },
                 )}
               >
